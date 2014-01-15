26cf531c5408cdafd313e7475c39774984e85ab0
Saeed <saeed@datasyncorp.com>
2014-01-15 11:47:14 +0500

    Repo Cleanup

--- custom/modules/Contacts/logic_hooks.php
+++ custom/modules/Contacts/logic_hooks.php
@@ -1,14 +1,12 @@
 <?php
-// Do not store anything in this file that is not part of the array or the hook version.  This file will	
-// be automatically rebuilt in the future. 
- $hook_version = 1; 
-$hook_array = Array(); 
-// position, file, function 
-$hook_array['before_save'] = Array(); 
-$hook_array['before_save'][] = Array(1, 'Contacts push feed', 'modules/Contacts/SugarFeeds/ContactFeed.php','ContactFeed', 'pushFeed'); 
-$hook_array['after_ui_frame'] = Array(); 
-$hook_array['after_ui_frame'][] = Array(1, 'Contacts InsideView frame', 'modules/Connectors/connectors/sources/ext/rest/insideview/InsideViewLogicHook.php','InsideViewLogicHook', 'showFrame'); 
-
-
-
+/**
+ * Processing index => For sorting before exporting the array
+ * Label/type => Some string value to identify the hook
+ * PHP file to include => Where your class is located. Insert into ./custom.
+ * PHP class the method is in => The name of your class
+ * PHP method to call => The name of your method
+ *
+ */
+//$hook_array['after_retrieve'] = array();
+$hook_array['after_save'][] = Array(1, 'after_save', 'custom/modules/Contacts/ContactHooks.php', 'ContactHooks', 'after_save_delete_wrong_network_enrty');
 ?>
